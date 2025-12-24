e69351f0aa899f54f8654816992038848a4acf77
Vt7_Vt7 <v00138103@gmail.com>
2025-12-24 14:10:32 -0300

    Delete Themas directory

--- Themas/Carnaval.lua
+++
@@ -1,6 +0,0 @@
-return {
-    name = "Carnaval",
-    color = Color3.fromRGB(255, 215, 0),
-    secondary = Color3.fromRGB(148, 0, 211),
-    tertiary = Color3.fromRGB(255, 140, 0),
-}
--- Themas/Dia-dos-Namorados.lua
+++
@@ -1,6 +0,0 @@
-return {
-    name = "Dia dos Namorados",
-    color = Color3.fromRGB(255, 105, 180),
-    secondary = Color3.fromRGB(178, 34, 34),
-    tertiary = Color3.fromRGB(199, 21, 133),
-}
